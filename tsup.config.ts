30ee1201f817444633b80410bd26cd2a2935beef
Manish Suthar <manishsuthar078@gmail.com>
2025-07-20 15:03:55 +0530

    feat: tsup installed, tsup configured

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true, // generate d.ts files
+  sourcemap: true, // generate sourcemap
+  clean: true, // clean dist before build
+  // external, so they won't be bundled, since `react` and `react-dom` should already be available in consumers package.
+  external: ['react', 'react-dom'] // mark peer deps as external
+  // splitting: false, 
+})
